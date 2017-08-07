e4b1796b1e0bd7abe09d25edf56ffa0c1e8ed5a6
Asiazdx <2081048066@qq.com>
2017-08-07 17:18:31 +0800

    commit

--- js/demo.js
+++
@@ -1,9 +0,0 @@
-$(function(){
-   for(var i=0;i<3;i++){
-      $(".navList")[i].onclick=function(){
-         $(".conList").hide();
-        var j=this.id.substr(4,1);
-         $(".conList").eq(j-1).show();
-      }
-   }
-})
